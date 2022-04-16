185b8dd12386d34e90c005a7aa5f5046e20f6f5b
karu samuel ngugi <karusamuelngugi@gmail.com>
2022-04-16 12:43:05 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { Runner } = require("mocha")
+
 // code your solution here
+function superbowlWin(arr){
+
+
+  var found =  arr.find((elem,index,arr)=>{
+        if(elem.result === "W"){
+            return elem
+        }
+
+    })
+    console.log(found)
+return JSON.parse(found).year;
+
+}
